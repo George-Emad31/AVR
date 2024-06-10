0227a64892df51272251f0bf06f50a129f917cb0
George-Emad <110574793+George-Emad31@users.noreply.github.com>
2024-06-10 13:07:16 +0300

    Add files via upload

---
+++ Temp sensor/GccApplication1/GccApplication1/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ADC.c
+
+button.c
+
+DIO.c
+
+keypad_driver.c
+
+LCD.c
+
+LED.c
+
+main.c
+
+seven segment.c
+
---
+++ Temp sensor/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,41 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 4/7/2024 7:37:14 PM
+ * Author : george
+ */
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include "ADC.h"
+ #include "App_config.h"
+u8 temperature ;
+int main(void)
+{   LCD_vInit();
+    ADC_init();
+    LCD_vSend_string("Temperature=");
+	
+   
+   while(1)
+   {	
+        
+       
+	temperature=(0.25*ADC_u16Read(0));
+	if (temperature<10)
+	{    
+		LCD_movecursor(1,13);
+		
+		LCD_vSend_char(temperature%10+ASCII);
+	    LCD_vSend_char(0xDF);// degree
+		LCD_vSend_char('c');
+        LCD_vSend_char(0x20);// space
+	} 
+	else if (temperature<100)
+	{   LCD_movecursor(1,13);
+		LCD_vSend_char(temperature/10 + ASCII );
+		LCD_vSend_char(temperature%10 + ASCII );
+		LCD_vSend_char(0xDF);// 0
+		LCD_vSend_char('c');
+	}
+	   
+   }
+}
